65254e531754dd60afbcab3bc289d55565e5d360
sbigelo <sean.bigelow1@gmail.com>
2021-07-16 14:31:31 -0400

    commit tree df54fff6325cfbfbf64a41cc9736d75048a8cd4f

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.handleClick} />
   </div>,
   document.getElementById('root')
 );
